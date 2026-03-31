808990f0514d98183ecb7e3bdac18aba76008745
whatprizrak <mr.priz1980@gmail.com>
2026-03-31 04:14:33 +0000

    массивы

---
+++ arrays.js
@@ -0,0 +1,44 @@
+const ocenki = [4, 5, 3, 5, 2, 4, 5, 3]
+const sum = ocenki.reduce((acc, n) => acc + n, 0)
+const avg = sum / ocenki.length
+console.log(avg.toFixed(2))
+
+
+function lol(value) {
+    return value > 2
+}
+const good = ocenki.filter(lol)
+const excel = good.map(good => `Оценка: ${good}`)
+console.log(excel)
+
+
+const fruits = ["банан", "яблоко", "вишня", "абрикос", "груша"]
+console.log(fruits.sort().join(", "))
+
+const products = [
+  { name: "Ноутбук", price: 75000 },
+  { name: "Мышь", price: 1500 },
+  { name: "Монитор", price: 32000 },
+  { name: "Клавиатура", price: 3500 },
+  { name: "Наушники", price: 8000 }
+]
+function prices(item) {
+    if (Number.isFinite(item.price) && item.price < 10000) {
+    return true;
+    }
+} 
+function names(item) {
+    return item.name
+}
+function summary(item) {
+    console.log(Number(item.price))
+    return Number(item.price)
+}
+console.log(products.filter(prices))
+console.log(products.map(names))
+console.log(products.reduce(summary))
+
+
+const soup = [12, 45, 7, 33, 18]
+console.log(soup.some(n => n > 40))
+console.log(soup.every(n => n > 5))
